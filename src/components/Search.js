390bd4495f3f3956d2513d157d8b205a81165014
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-02-06 21:19:02 +0900

    feat: 중간 부분 구현

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,17 +6,27 @@ const Main = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  font-size: 30px;
+`;
+const MainInner = styled.div`
+  width: 70%;
+`;
+const Input = styled.input`
+  width: 300px;
+  border: none;
+  background-color: #d9d9d9;
+  font-size: 20px;
 `;
 
 const Above = styled.div`
-  width: 70%;
+  height: 250px;
 `;
 const Toggle = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
 
-  font-size: 30px;
   padding-bottom: 20px;
 `;
 const SameOrDifferent = styled.div`
@@ -33,30 +43,55 @@ const SameBtn = styled.button`
   border: none;
   border-radius: 20px;
   cursor: pointer;
+
+  font-size: 20px;
 `;
 const DiffBtn = styled.button`
   width: 200px;
   border: none;
   border-radius: 20px;
   cursor: pointer;
+
+  font-size: 20px;
 `;
-const SearchBtn = styled.button`
-  width: 100px;
-  background-color: white;
-  border: 2px solid black;
-  border-radius: 20px;
-  cursor: pointer;
-`;
+const SearchBtn = styled(DiffBtn)``;
 const Inputinfo = styled.div``;
 const SameFlight = styled.div``;
-const Same_Above = styled.div`
+const SameAbove = styled.div`
   display: flex;
+  justify-content: space-between;
+  padding-bottom: 20px;
+  height: 40px;
 `;
-const Same_Below = styled.div``;
+
+const SameBelow = styled.div`
+  display: flex;
+  height: 40px;
+`;
+
 const DiffFlight = styled.div``;
+const DiffAbove = styled(SameAbove)``;
+const DiffMiddle = styled(SameAbove)``;
+const DiffBelow = styled(SameBelow)``;
+
+const Middle = styled.div`
+  display: flex;
+  justify-content: center;
+
+  height: 500px;
+`;
+const Sentence = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  text-align: center;
+`;
+
+const Bottom = styled.div``;
 
 function Search() {
-  const [isToggle, setIsToggle] = useState(false); // 동일한 항공사
+  const [isToggle, setIsToggle] = useState(true); // 동일한 항공사
   const DiffToggle = () => {
     setIsToggle(false);
   };
@@ -67,52 +102,67 @@ function Search() {
 
   return (
     <Main>
-      <Above>
-        <Toggle>
-          <SameOrDifferent>
-            {isToggle ? "동일한 항공사" : "다른 항공사"}
-          </SameOrDifferent>
-          <ToggleBtns>
-            <SameBtn
-              onClick={SameToggle}
-              style={{ backgroundColor: isToggle ? "white" : "transparent" }}
-            >
-              동일한 항공사
-            </SameBtn>
-            <DiffBtn
-              onClick={DiffToggle}
-              style={{ backgroundColor: isToggle ? "transparent" : "white" }}
-            >
-              다른 항공사
-            </DiffBtn>
-          </ToggleBtns>
-          <SearchBtn>조회</SearchBtn>
-        </Toggle>
-        <Inputinfo>
-          {isToggle ? (
-            <SameFlight>
-              <Same_Above>
-                <div>항공사</div>
-                <div>예약 번호</div>
-                <div>출국일</div>
-              </Same_Above>
-              <Same_Below>
-                <div>여행자 성</div>
-              </Same_Below>
-            </SameFlight>
-          ) : (
-            <DiffFlight>
-              <div>출발 항공사</div>
-              <div>예약 번호 (출발)</div>
-              <div>출국일</div>
-              <div>여행자 성</div>
-              <div>경유 항공사</div>
-              <div>예약 번호(경유)</div>
-              <div>출국일(경유)</div>
-            </DiffFlight>
-          )}
-        </Inputinfo>
-      </Above>
+      <MainInner>
+        <Above>
+          <Toggle>
+            <SameOrDifferent>
+              {isToggle ? "동일한 항공사" : "다른 항공사"}
+            </SameOrDifferent>
+            <ToggleBtns>
+              <SameBtn
+                onClick={SameToggle}
+                style={{ backgroundColor: isToggle ? "white" : "transparent" }}
+              >
+                동일한 항공사
+              </SameBtn>
+              <DiffBtn
+                onClick={DiffToggle}
+                style={{ backgroundColor: isToggle ? "transparent" : "white" }}
+              >
+                다른 항공사
+              </DiffBtn>
+            </ToggleBtns>
+            <SearchBtn>조회</SearchBtn>
+          </Toggle>
+          <Inputinfo>
+            {isToggle ? (
+              <SameFlight>
+                <SameAbove>
+                  <Input placeholder="항공사"></Input>
+                  <Input placeholder="예약 번호"></Input>
+                  <Input placeholder="출국일" type="date"></Input>
+                </SameAbove>
+                <SameBelow>
+                  <Input placeholder="여행자 성"></Input>
+                </SameBelow>
+              </SameFlight>
+            ) : (
+              <DiffFlight>
+                <DiffAbove>
+                  <Input placeholder="출발 항공사"></Input>
+                  <Input placeholder="예약 번호 (출발)"></Input>
+                  <Input placeholder="출국일"></Input>
+                </DiffAbove>
+                <DiffMiddle>
+                  <Input placeholder="출국일(경유)"></Input>
+                  <Input placeholder="경유 항공사"></Input>
+                  <Input placeholder="예약 번호(경유)"></Input>
+                </DiffMiddle>
+                <DiffBelow>
+                  <Input placeholder="여행자 성"></Input>
+                </DiffBelow>
+              </DiffFlight>
+            )}
+          </Inputinfo>
+        </Above>
+        <Middle>
+          <Sentence>
+            <div>여행의 시작과 끝</div>
+            <div>모든 순간이 의미있게</div>
+          </Sentence>
+        </Middle>
+        <Bottom></Bottom>
+      </MainInner>
     </Main>
   );
 }
