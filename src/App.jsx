bb63e7a242bcf1eefce59e70ed781dfc7b27ad7c
Jang <hansung.j1106@gmail.com>
2024-02-15 21:25:08 +0900

    (edit)-App.jsx 파일 main branch 형태로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Schedule from "./components/Schedule";
 import Search from "./components/Search";
-import Banner from "./components/SearchPage/Banner";
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path={process.env.PUBLIC_URL + "/"} element={<Search />} />
-        <Route path={process.env.PUBLIC_URL + "/banner"} element={<Banner />} />
-
         {/* <Route
         path={process.env.PUBLIC_URL + "/schedule"}
         element={<Schedule />}
