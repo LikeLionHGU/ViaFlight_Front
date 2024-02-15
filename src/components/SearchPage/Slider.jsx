97091b8d3feee9d4e15c5e5d2baf2277e5bd7771
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-02-15 17:33:48 +0900

    style: 폰트의 사이즈, weight 정리

--- src/components/SearchPage/Slider.jsx
+++ src/components/SearchPage/Slider.jsx
@@ -1,32 +1,84 @@
 import React from "react";
 import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode, Pagination, Navigation, Autoplay } from "swiper/modules";
+import { FreeMode, Pagination, Autoplay } from "swiper/modules";
 import SwiperCore from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
-import "../style/Slider.css";
+import "../../style/Slider.css";
 
-import { ServiceData } from "../SliderConstant/index";
+import { ServiceData } from "./SliderContents";
 
 // Initialize required modules
-SwiperCore.use([FreeMode, Pagination, Navigation, Autoplay]);
+SwiperCore.use([FreeMode, Pagination, Autoplay]);
 
-const StyledContainer1 = styled.div`
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  color: #ec4d27;
+`;
+
+const Title = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+
+  padding: 30px 0px;
+`;
+
+const TitleMain = styled.div`
+  padding-right: 30px;
+
+  font-size: 40px;
+  font-weight: 500;
+`;
+const TitleSub = styled.div`
+  font-size: 20px;
+  font-weight: 400;
+`;
+
+const Message = styled.div`
+  padding: 30px 0;
+
+  font-size: 160px;
+  font-weight: 700;
+`;
+
+const Contents = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  padding-right: 50px;
+
+  font-size: 22px;
+  font-weight: 500;
+`;
+
+const UpBtn = styled.div`
+  display: flex;
   flex-direction: column;
-  height: 100vh;
-  z-index: 0;
+  align-items: center;
+
+  padding: 100px;
+`;
+
+const UpBtnStyled = styled.button`
+  padding: 15px 40px;
+  border: none;
+  border-radius: 5px;
+
+  background-color: #ec4d27;
+  color: white;
 `;
 
 function Slider() {
   return (
-    <StyledContainer1>
-      <div className="swiper-button-prev"></div>
+    <StyledContainer>
+      <Title>
+        <TitleMain>VIA FLIGHT FAQS</TitleMain>
+        <TitleSub>Relieving discomfort and fear</TitleSub>
+      </Title>
       <Swiper
         style={{ margin: "0" }}
         breakpoints={{
@@ -45,12 +97,8 @@ function Slider() {
           clickable: true,
           // el: ".swiper-custom-pagination",
         }}
-        navigation={{
-          // 위치 변환 버튼
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        }}
         autoplay={{ delay: 3000, disableOnInteraction: false }}
+        slideToClickedSlide={true}
         className="swiper"
       >
         {ServiceData.map((item, index) => (
@@ -71,8 +119,27 @@ function Slider() {
           </SwiperSlide>
         ))}
       </Swiper>
-      <div className="swiper-button-next"></div>
-    </StyledContainer1>
+      <Message>
+        Discover
+        <br /> New Journey
+      </Message>
+      <Contents>
+        <div>
+          경유지 정보만 입력하면, 경유지에서의 대기 시간별로 맞춤화 된 투어
+          코스,
+          <br />
+          액티비티를 추천드립니다. 여행의 모든 순간을 알차게 구성해보세요!
+          <br />
+          경유 시간이 짧아도 걱정하지 마세요. Via Flight와 함께라면 공항도
+          여행지입니다.
+          <br />
+          공항 관련 각종 정보, 이벤트, 기획전 등의 정보를 찾아보세요.
+        </div>
+      </Contents>
+      <UpBtn>
+        <UpBtnStyled>경유지 정보 입력하러 가기</UpBtnStyled>
+      </UpBtn>
+    </StyledContainer>
   );
 }
 
---
+++ src/components/SearchPage/SliderContents.js
@@ -0,0 +1,54 @@
+import { RxCrop, RxDesktop, RxPencil2 } from "react-icons/rx";
+
+import travel1 from "../img/img1.jpeg";
+import travel2 from "../img/img2.jpg";
+import travel3 from "../img/img3.jpg";
+import travel4 from "../img/img4.jpg";
+import travel5 from "../img/img5.jpeg";
+import travel6 from "../img/img6.jpg";
+import travel7 from "../img/img7.jpg";
+
+export const ServiceData = [
+  {
+    icon: RxCrop,
+    title: "Test1",
+    content: "Lorem is bla blalblblblblblblb ",
+    backgroundImg: travel1,
+  },
+  {
+    icon: RxDesktop,
+    title: "Test2",
+    content: "Lorem is bla  claclclclcclclclc ",
+    backgroundImg: travel2,
+  },
+  {
+    icon: RxPencil2,
+    title: "Test3",
+    content: "Lorem is bla dladldldldldldld ",
+    backgroundImg: travel3,
+  },
+  {
+    icon: RxPencil2,
+    title: "Test4",
+    content: "Lorem is bla dladldldldldldld ",
+    backgroundImg: travel4,
+  },
+  {
+    icon: RxPencil2,
+    title: "Test5",
+    content: "Lorem is bla dladldldldldldld ",
+    backgroundImg: travel5,
+  },
+  {
+    icon: RxPencil2,
+    title: "Test6",
+    content: "Lorem is bla dladldldldldldld ",
+    backgroundImg: travel6,
+  },
+  {
+    icon: RxPencil2,
+    title: "Test7",
+    content: "Lorem is bla dladldldldldldld ",
+    backgroundImg: travel7,
+  },
+];
