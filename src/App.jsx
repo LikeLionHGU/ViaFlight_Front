11bec6c4adfb03d9fd90a371aee0305afda20b07
Seonghwan Sim <130568725+hwan129@users.noreply.github.com>
2024-02-17 03:02:04 +0900

    style: 정보 입력

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Schedule from "./components/Schedule";
-import Search from "./components/Search";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path={process.env.PUBLIC_URL + "/"} element={<Search />} />
+        <Route path={process.env.PUBLIC_URL + "/"} element={<Footer/>} />
         {/* <Route
         path={process.env.PUBLIC_URL + "/schedule"}
         element={<Schedule />}
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,10 +1,23 @@
+import styled from "styled-components";
 import FooterLogo from "../../img/FooterLogo.svg";
+
+const StyleContainer = styled.div`
+  background-color: black;
+`;
 
 function Footer() {
   return (
-    <>
+    <StyleContainer>
       <img src={FooterLogo} alt="logo" />
-    </>
+      <div>내 경유지 조회</div>
+      <div>맞춤형 여행 일정</div>
+      <div>ABOUT US</div>
+      <div>© ViaFlight - 2024</div>
+      <div>instagram</div>
+      <div>facebook</div>
+      <div>blog</div>
+      <div>All rights reserved.</div>
+    </StyleContainer>
   );
 }
 
