15b11611f4ac3fe06e2d30e295de9e76179a2a78
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-02-18 23:37:28 +0900

    feat: 토글 기능 구현

---
+++ src/components/SchedulePage/Changing/ToggleBtn.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const ToggleSlider = styled.span`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  border-radius: 34px;
+
+  &:before {
+    position: absolute;
+    content: "";
+    height: 15px;
+    width: 15px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+    border-radius: 50%;
+  }
+`;
+
+const ToggleContainer = styled.div`
+  position: relative;
+  margin-top: 8rem;
+  left: 47%;
+  cursor: pointer;
+
+  > .toggle-container {
+    width: 150px;
+    height: 60px;
+    border-radius: 30px;
+    background-color: rgb(233, 233, 234);
+  }
+  //.toggle--checked 클래스가 활성화 되었을 경우의 CSS를 구현
+  > .toggle--checked {
+    background-color: rgb(0, 200, 102);
+    transition: 1s;
+  }
+
+  > .toggle-circle {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+    background-color: rgb(255, 254, 255);
+    transition: 1s;
+    //.toggle--checked 클래스가 활성화 되었을 경우의 CSS를 구현
+  }
+  > .toggle--checked {
+    left: 92px;
+    transition: 1s;
+  }
+`;
+
+export default function ToggleBtn() {
+  const [isOn, setisOn] = useState(false);
+
+  const toggleHandler = () => {
+    // isOn의 상태를 변경하는 메소드를 구현
+    setisOn(!isOn);
+  };
+
+  return (
+    <>
+      <ToggleContainer
+        // 클릭하면 토글이 켜진 상태(isOn)를 boolean 타입으로 변경하는 메소드가 실행
+        onClick={toggleHandler}
+      >
+        {/* 아래에 div 엘리먼트 2개가 있다. 각각의 클래스를 'toggle-container', 'toggle-circle' 로 지정 */}
+        {/* Toggle Switch가 ON인 상태일 경우에만 toggle--checked 클래스를 div 엘리먼트 2개에 모두 추가. 조건부 스타일링을 활용*/}
+        <div
+          className={`toggle-container ${isOn ? "toggle--checked" : null}`}
+        />
+        <div className={`toggle-circle ${isOn ? "toggle--checked" : null}`} />
+      </ToggleContainer>
+      {/* Desc 컴포넌트를 활용*/}
+      {/* Toggle Switch가 ON인 상태일 경우에 Desc 컴포넌트 내부의 텍스트를 'Toggle Switch ON'으로, 그렇지 않은 경우 'Toggle Switch OFF'. 조건부 렌더링을 활용. */}
+    </>
+  );
+}
