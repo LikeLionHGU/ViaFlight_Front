deebcc4dbe130f0586bc7209bf14da864470eff2
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-02-19 22:15:29 +0900

    style: 아이콘 추가

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import InAirport from "./SchedulePage/InAirport";
 import ToggleBtn from "./SchedulePage/Changing/ToggleBtn";
 // import Header from "./layout/Header";
-import Schedule_Header from "./SchedulePage/NoChanging/Schedule_Header";
+import ScheduleHeader from "./SchedulePage/NoChanging/Schedule_Header";
 function Schedule() {
   return (
     <>
-      {/* <Header /> */}
-      <Schedule_Header />
+      <ScheduleHeader />
       <ToggleBtn />
       <InAirport />;
     </>
---
+++ src/components/SchedulePage/Changing/SimpleInfo.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import WifiIcon from "../../../img/wifi.svg";
+import InfoIcon from "../../../img/info.svg";
+import CigaretteIcon from "../../../img/cigarette.svg";
+import CallIcon from "../../../img/call.svg";
+import ShowerIcon from "../../../img/shower.svg";
+
+const Main = styled.div`
+  display: flex;
+`;
+
+const Wifi = styled.img``;
+const Info = styled.img``;
+const Cigarette = styled.img``;
+const Call = styled.img``;
+const Shower = styled.img``;
+
+export default function SimpleInfo() {
+  return (
+    <Main>
+      <Wifi src={WifiIcon} />
+      <Info src={InfoIcon} />
+      <Cigarette src={CigaretteIcon} />
+      <Call src={CallIcon} />
+      <Shower src={ShowerIcon} />
+    </Main>
+  );
+}
