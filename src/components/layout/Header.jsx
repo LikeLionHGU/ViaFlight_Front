cd1a40b5eb8396f19d6e437ea33409d22dc170c8
Jang <hansung.j1106@gmail.com>
2024-02-19 11:14:23 +0900

    (fix)-페이지 선택에 따라서  header 색상 변경 기능 (동기적 수정)

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../style/header.css";
 import { Link } from "react-router-dom";
 // colorCheck === true일 때, (조회 페이지) [붉은 색상]
@@ -10,14 +10,28 @@ import Navigation_bar from "../../img/Navigation_Bar_Line.png";
 import S_header_Logo from "../../img/Schedule_header_Logo.png";
 import S_Earth_Icon from "../../img/Schedule_Earth_Icon.png";
 import S_Navigation_bar from "../../img/Schedule_Navigation_Bar_Line.png";
-// import Navigation_Bar_line from "../../img/Navigation_Bar_Line.png";
 
 // Header BTN 영역(Button 역할)
 
-function Location() {
+function Header() {
+  //localstorage가 empty인 경우 확인(입력 유도 페이지 결정하는 변수)
+  // const [emptyCheck, setCheck] = useState(true);
+
+  //true: #2c6e49(조회 페이지); false: #ec4d27(schedule 페이지)
+  //[true: 주황, fasle: 녹색]
+  const [colorCheck, setColor] = useState(true);
+
+  //localstorage에 저장된 입력값 불러오기
+  // const savedAirport = localStorage.getItem("viaAirport");
+  // const savedATime = localStorage.getItem("arrivalTime");
+  // const savedDTime = localStorage.getItem("durationTime");
+
+  // paige에 따라서 Header 색상 변경 확인하는 함수
+  const savedColor = localStorage.getItem("Headercolor");
+
+  // 위치 정보 받아오는 함수
   const [lat, setlat] = useState("");
   const [lon, setlon] = useState("");
-
   function onGeoOk(position) {
     setlat(position.coords.latitude); // 위도 정보 추출
     setlon(position.coords.longitude); // 경도 정보 추출
@@ -26,54 +40,36 @@ function Location() {
     alert("Can't find you. No location for you");
   }
 
-  const savedColor = localStorage.getItem("Headercolor");
+  //localStorage에 입력 여부에 따라서, 유도페이지 연결 [작동은 되지만, 비동기 처리로 밀림... ] -> sol, useEffect 예정
+  // const onEmptyCheck = () => {
+  //   if (savedAirport === null && savedATime === null && savedDTime === null) {
+  //     setCheck(false);
+  //   } else {
+  //     setCheck(true);
+  //   }
+  // };
 
-  return (
-    <div className="location">
-      {navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError)}
-      <div className="location_icon">
-        <img src={!savedColor ? Earth_Icon : S_Earth_Icon}></img>
-      </div>
-      <div className="location_text">
-        {typeof lat === "number" ? lat.toFixed(4) : ""}° N,
-        {typeof lon === "number" ? lon.toFixed(4) : ""}° E
-      </div>
-    </div>
-  );
-}
+  /*새로 추가된 부분*/
+  useEffect(() => {
+    // Check localStorage to determine colorCheck when component mounts
+    const savedColor = localStorage.getItem("Headercolor");
+    setColor(savedColor === "true");
+  }, []);
 
-function Header() {
-  //localstorage가 empty인 경우 확인(입력 유도 페이지 결정하는 변수)
-  const [emptyCheck, setCheck] = useState(true);
-
-  //true: #ec4d27(조회 페이지); false: #2c6e49(schedule 페이지)
-  const [colorCheck, setColor] = useState(true);
-
-  //localstorage에 저장된 입력값 불러오기
-  const savedAirport = localStorage.getItem("viaAirport");
-  const savedATime = localStorage.getItem("arrivalTime");
-  const savedDTime = localStorage.getItem("durationTime");
-
-  const savedColor = localStorage.getItem("Headercolor");
-  //localStorage에 입력 여부에 따라서, 유도페이지 연결 [작동은 되지만, 비동기 처리로 밀림... ]
-  const onEmptyCheck = () => {
-    if (savedAirport === null && savedATime === null && savedDTime === null) {
-      setCheck(false);
-    } else {
-      setCheck(true);
-    }
-  };
-
+  /*맞춤 페이지 눌렀을 때 */
   const onColorSchedule = () => {
     if (savedColor !== null) localStorage.removeItem("Headercolor");
-    setColor(false);
     localStorage.setItem("Headercolor", false);
+
+    setColor(false);
   };
 
+  /*조회 페이지 눌렀을 때 */
   const onColorSearch = () => {
     if (savedColor !== null) localStorage.removeItem("Headercolor");
-    setColor(true);
     localStorage.setItem("Headercolor", true);
+
+    setColor(true);
   };
 
   return (
@@ -84,63 +80,61 @@ function Header() {
         {/* location색상 줄 수 있음 */}
         <div
           className="header-BTN"
-          style={{ color: !savedColor ? "#ec4d27" : "#2c6e49" }}
+          style={{ color: !colorCheck ? "#2c6e49" : "#ec4d27" }}
         >
           {/* Header Logo 부분 */}
           <div className="header_logo">
             <img
               className="img_logo"
-              src={!savedColor ? header_Logo : S_header_Logo}
+              src={!colorCheck ? S_header_Logo : header_Logo}
               alt="via-flight-logo"
             ></img>
           </div>
-          {/* <div className="className=" ticket_link> </div> */}
           <Link
             to={`/`}
             className="ticket_link"
             onClick={onColorSearch}
-            style={{ color: !savedColor ? "#ec4d27" : "#2c6e49" }}
+            style={{ color: !colorCheck ? "#2c6e49" : "#ec4d27" }}
           >
-            {/* <NavigtionSpan id="ticket"> */}
             <div className="ticket_text">내 경유지 조회</div>
-            {/* </NavigtionSpan> */}
           </Link>
-          {/* <div className="className=" schedule_link></div> */}
-          {/* {console.log(emptyCheck)} */}
-          {console.log(colorCheck)}
+
           <Link
+            to={`/schedule`}
             // to={emptyCheck === true ? `/schedule` : `/input`} //true: 입력 페이지로, flase: 유도 페이지로
             className="schedule_link"
             // onClick={onEmptyCheck}
             onClick={onColorSchedule}
-            style={{ color: !savedColor ? "#ec4d27" : "#2c6e49" }}
+            style={{ color: !colorCheck ? "#2c6e49" : "#ec4d27" }}
           >
-            {/* <NavigtionSpan id="schedule"> */}
             <div className="schedule_text">맞춤형 여행 일정</div>
-            {/* </NavigtionSpan> */}
           </Link>
-          {/* <div className="className=" about_link></div> */}
           <Link
             to={`/about`}
             className="about_link"
-            style={{ color: !savedColor ? "#ec4d27" : "#2c6e49" }}
+            style={{ color: !colorCheck ? "#2c6e49" : "#ec4d27" }}
           >
-            {/* <NavigtionSpan id="about"> */}
             <div className="about_text">ABOUT US</div>
-            {/* </NavigtionSpan> */}
           </Link>
 
-          {/* <NavigtionSpan id="header_location"> */}
-          <Location></Location>
-          {/* </NavigtionSpan> */}
+          <div className="location">
+            {navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError)}
+            <div className="location_icon">
+              <img src={!colorCheck ? S_Earth_Icon : Earth_Icon}></img>
+            </div>
+            <div className="location_text">
+              {typeof lat === "number" ? lat.toFixed(4) : ""}° N,
+              {typeof lon === "number" ? lon.toFixed(4) : ""}° E
+            </div>
+          </div>
         </div>
       </div>
       <div
         className="Navigation_bar_line_div"
         style={{
-          backgroundImage: !savedColor
-            ? `url(${Navigation_bar})`
-            : `url(${S_Navigation_bar})`,
+          backgroundImage: !colorCheck
+            ? `url(${S_Navigation_bar})`
+            : `url(${Navigation_bar})`,
         }}
       ></div>
     </div>
