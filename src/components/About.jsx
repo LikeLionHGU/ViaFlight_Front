73866eba3ba18bfc0037609744b4c0c76fc56a1d
Jang <hansung.j1106@gmail.com>
2024-02-20 19:35:52 +0900

    (feat)-about페이지 wire - frame

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import About_banner from "./AboutPage/About_banner";
+import About_grid1 from "./AboutPage/About_grid";
+import Header from "../components/layout/Header";
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <Header />
+      <About_banner />
+      <About_grid1 />
+    </>
+  );
 }
 
 export default About;
