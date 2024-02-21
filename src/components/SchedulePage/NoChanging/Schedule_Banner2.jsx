f460552d24cf29327928ae596d0efabe3d4b6e70
Jang <hansung.j1106@gmail.com>
2024-02-21 15:13:14 +0900

    (feat)-sticky효과, 페이지 fadein, header색상 변경 효과 구현

--- src/components/SchedulePage/NoChanging/Schedule_Banner2.jsx
+++ src/components/SchedulePage/NoChanging/Schedule_Banner2.jsx
@@ -7,11 +7,14 @@ function Schedule_Banner2() {
   return (
     <>
       <div className="Schedule_Frame_Banner">
-        <div className="Schedule_Frame_Banner_Icons">
-          <SimpleInfo />
-        </div>
-        <div className="Schedule_Frame_Banner_infoWeather">
-          <Schedule_Timebox /> {/*시간 정보*/}
+        <div className="Schedule_Frame_Banner_infoBox">
+          {/* sticky 효과를 위한 div */}
+          <div className="Schedule_Frame_Banner_Icons">
+            <SimpleInfo />
+          </div>
+          <div className="Schedule_Frame_Banner_infoWeather">
+            <Schedule_Timebox /> {/*시간 정보*/}
+          </div>
         </div>
       </div>
     </>
