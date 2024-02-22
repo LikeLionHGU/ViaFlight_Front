3a9f3119d6362b2f020f03fe0f06a693bee70b36
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-02-22 20:35:39 +0900

    style: 교통편 설명

--- src/components/SchedulePage/Changing/InTraffic.jsx
+++ src/components/SchedulePage/Changing/InTraffic.jsx
@@ -6,25 +6,68 @@ import Fadein from "../../AboutPage/About_fadin";
 const StyleContainer = styled.div`
   background-color: black;
   color: #ffffff;
+  padding: 0 4.5%;
 `;
 
-const InTrain = styled.img``;
+const Main = styled.div`
+  display: flex;
 
-const InBus = styled.img``;
+  border-top: 1px solid #ffffff;
+  border-bottom: 1px solid #ffffff;
+  padding: 85px 0;
+`;
+
+const Left = styled.div`
+  font-size: 35px;
+`;
+
+const Right = styled.div`
+  width: 65%;
+
+  font-size: 18px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const InTrafficImg = styled.img`
+  padding-bottom: 15px;
+`;
+
+const InTrainCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const InBusCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding-left: 130px;
+`;
+
+const InTrainText = styled.div``;
+
+const InBusText = styled.div``;
 
 export default function InTraffic() {
   return (
     <StyleContainer>
       <Fadein>
-        <div>공항 터미널 이동 정보</div>
-        <div>
-          <InTrain src={InTrainImg} />
-          <div>Sky Train</div>
-        </div>
-        <div>
-          <InBus src={InBusImg} />
-          <div>Bus Shuttle Service</div>
-        </div>
+        <Main>
+          <Left>공항 터미널 이동 정보</Left>
+          <Right>
+            <InTrainCard>
+              <InTrafficImg src={InTrainImg} />
+              <InTrainText>Sky Train</InTrainText>
+            </InTrainCard>
+            <InBusCard>
+              <InTrafficImg src={InBusImg} />
+              <InBusText>Bus Shuttle Service</InBusText>
+            </InBusCard>
+          </Right>
+        </Main>
       </Fadein>
     </StyleContainer>
   );
