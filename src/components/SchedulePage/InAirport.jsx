4b6ab0a75e6a42632c280801b99a21d463b43319
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-02-22 20:50:31 +0900

    파일정리

--- src/components/SchedulePage/InAirport.jsx
+++ src/components/SchedulePage/InAirport.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ScheduleBanner1 from "./NoChanging/Schedule_Banner1";
 import ScheduleBanner2 from "./NoChanging/Schedule_Banner2";
 import ScheduleGridGreen from "./NoChanging/Schedule_Grid_Green";
-import ScheduleSlider from "./Changing/InAirport/InView/ScheduleSlider";
+import ScheduleSlider from "./Changing/InAirport/ScheduleSlider";
 import InTraffic from "./Changing/InAirport/InView/InTraffic";
 
 import Fadein from "../AboutPage/About_fadin"; //fadein ȿ��
