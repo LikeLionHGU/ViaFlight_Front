e35fe1d121f6c32f6e5811d50b9ce6b6400401d9
Jang, Hansome <143300013+HansungJang@users.noreply.github.com>
2024-02-22 23:08:48 +0900

    Update App.jsx
    App.jsx 파일 원상 복구

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,11 @@ import Search from "./components/Search";
 import Schedule from "./components/Schedule";
 import About from "./components/About";
 
-import TimeBox from "./components/SchedulePage/NoChanging/Schedule_Clock/Schedule_Timebox";
 
 function App() {
   return (
     <Router>
-      {/* <Routes>
+       <Routes>
         <Route path={process.env.PUBLIC_URL + "/"} element={<Search />} />
         <Route
           path={process.env.PUBLIC_URL + "/schedule"}
@@ -17,10 +16,7 @@ function App() {
         />
         <Route path={process.env.PUBLIC_URL + "/about"} element={<About />} />
       </Routes>
-      <Footer /> */}
-      <Routes>
-        <Route path={process.env.PUBLIC_URL + "/"} element={<TimeBox />} />
-      </Routes>
+      <Footer /> 
     </Router>
   );
 }
