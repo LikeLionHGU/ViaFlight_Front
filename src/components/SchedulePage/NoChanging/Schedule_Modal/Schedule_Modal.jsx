7c2710ea4106d6a0367bf447b79abd4825682552
Jang, Hansome <143300013+HansungJang@users.noreply.github.com>
2024-02-23 14:43:55 +0900

    Revert "Revert "Revert "Hansung #99/search modal"""

--- src/components/SchedulePage/NoChanging/Schedule_Modal/Schedule_Modal.jsx
+++ src/components/SchedulePage/NoChanging/Schedule_Modal/Schedule_Modal.jsx
@@ -2,19 +2,23 @@ import { React, useState } from "react";
 import Modal from "react-modal";
 import "../../../../style/Schedule_modal.css";
 
-function Schedule_Modal({ tittle, description, Modalicon, ModaliconBlack }) {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+import Shower_Icon from "../../../../img/shower.svg";
+
+function Schedule_Modal({ showerFacilities }) {
+  //추후에 props로 넘겨받을 데이터 저장하는 변수들
+  const title = "SHOWER FACILITIES";
+  const [modalIsOpen, setModalIsOpen] = useState(false); //modal이 클릭 되었는지 확인하는 변수
 
   const customStyles = {
     overlay: {
       zIndex: 1000,
-      backgroundColor: "#00000080",
+      backgroundColor: "#00000070",
     },
     content: {
-      width: "1200px",
-      height: "799px",
+      width: "892px",
+      height: "594px",
       inset: "unset",
-      margin: "55vh auto",
+      margin: "50vh auto",
       padding: 0,
       transform: "translateY(-50%)",
       position: "relative",
@@ -25,7 +29,12 @@ function Schedule_Modal({ tittle, description, Modalicon, ModaliconBlack }) {
     <div className="list-container">
       {/* Before Modal, <div> */}
       <div className="rect3" onClick={() => setModalIsOpen(true)}>
-        <img src={Modalicon} alt=" ICON이 들어갈 자리 "></img>
+        {/*클릭시 Modal open*/}
+        <img
+          src={Shower_Icon}
+          alt="  ICON이 들어갈 자리 
+"
+        ></img>
       </div>
       {/* After Modal(Modal 화면) */}
       <Modal
@@ -33,18 +42,14 @@ function Schedule_Modal({ tittle, description, Modalicon, ModaliconBlack }) {
         onRequestClose={() => setModalIsOpen(false)}
         style={customStyles}
       >
-        <button
-          onClick={() => setModalIsOpen(false)}
-          className="closeBtn_container"
-        >
-          <div className="closeBtn"> </div>
-        </button>
-
+        <div className="closeBtn">
+          <button onClick={() => setModalIsOpen(false)}></button>
+        </div>
         <div className="modal-wrapper">
-          <img className="Info_Icon" src={ModaliconBlack}></img>
-          <div className="Info_title">{tittle}</div>
+          <div className="Info_Icon"></div>
+          <div className="Info_title">{title}</div>
           <div className="Info_descrip">
-            <p>{description}</p>
+            <p>{showerFacilities}</p>
           </div>
         </div>
       </Modal>
