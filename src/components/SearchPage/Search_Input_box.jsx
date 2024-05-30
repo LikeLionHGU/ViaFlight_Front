05f994cff7dc8d8d901301bb8b9f0e365c14f4e1
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-05-30 22:57:42 +0900

    예외 사항 변경

--- src/components/SearchPage/Search_Input_box.jsx
+++ src/components/SearchPage/Search_Input_box.jsx
@@ -67,7 +67,7 @@ function Search_Input_box() {
     }
 
     //창이 공항 이외 정보 입력을 넣는 경우
-    if (airport !== "싱가포르 창이 공항(SIN)") {
+    if (airport !== "싱가포르창이공항(SIN)") {
       e.preventDefault();
 
       alert(`${airport} 정보는  준비중입니다:)`);
